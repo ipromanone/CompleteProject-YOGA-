09ff027d674e957fbf242a889886400ab69c810c
Roman <ipromanone@gmail.com>
2020-12-24 19:11:02 +0300

    Wabpack

---
+++ src/js/parts/form.js
@@ -0,0 +1,49 @@
+function myForm() {
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        let formData = new FormData(form);
+        
+
+        let obj = {};
+        formData.forEach(function(value, key) { //Превращаем в 
+            obj[key] = value;                   // JSON данные
+        });
+        let json = JSON.stringify(obj);
+
+
+        request.send(json);
+        request.addEventListener('readystatechange', function() {
+            if(request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+
+    });
+}
+module.exports = myForm;
---
+++ src/js/parts/modal.js
@@ -0,0 +1,20 @@
+function myModal() {
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+        document.body.style.overflow = 'hidden';//Запрещает прокрутку страницы пока открыто модальное окно
+
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+} 
+module.exports = myModal;
